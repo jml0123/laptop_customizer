91fb708767be13cbf0d5d9c617eb2ac38b77d017
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-05-05 15:22:05 -0400

    remove stray dir

--- .src/Parts/Parts.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Parts from './Parts';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Parts />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
